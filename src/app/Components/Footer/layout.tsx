ccb57b465e59083862f79757839cf25aa512953c
Suranjana Roy <roysupra75@gmail.com>
2025-01-27 22:28:07 +0530

    added the project

---
+++ src/app/Components/Footer/layout.tsx
@@ -0,0 +1,16 @@
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={` antialiased`}
+        >
+        
+        {children}
+      </body>
+    </html>
+  );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import Navbar from "./Components/Navbar/page";
+import Footer from "./Components/Footer/page";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,9 +15,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+        className={` antialiased`}
+        >
+        <Navbar/>
         {children}
+        <Footer/>
       </body>
     </html>
   );
